c3253da1435ad281bbf017fb1f103b14231f0cc5
kiranreddy30 <pkiransai487@gmail.com>
2023-08-15 02:02:39 +0530

    f

--- client/src/components/shared/Form/InputType.js
+++ client/src/components/shared/Form/InputType.js
@@ -1,23 +1,29 @@
-import React from 'react'
+import React from "react";
 
-const InputType = ({labelText,labelFor,inputType,name,value,onChange}) => {
+const InputType = ({
+  labelText,
+  labelFor,
+  inputType,
+  value,
+  onChange,
+  name,
+}) => {
   return (
     <>
-      
-      <div class="mb-3">
-            <label className='form-label' htmlFor={labelFor}>{labelText}</label>
-            <input 
-            type={inputType} 
-            className="form-control" 
-            name={name}
-            value={value}
-            onChange={onChange} 
-           />
+      <div className="mb-1">
+        <label htmlFor={labelFor} className="form-label">
+          {labelText}
+        </label>
+        <input
+          type={inputType}
+          className="form-control"
+          name={name}
+          value={value}
+          onChange={onChange}
+        />
       </div>
-    
-    
     </>
-  )
-}
+  );
+};
 
-export default InputType
+export default InputType;
